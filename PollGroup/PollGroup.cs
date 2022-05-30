c935314a6512c12a54cf9c37f74624a4f4dc85e2
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-05-30 12:24:54 -0700

    Adds more poll options for debugging

--- PollGroup/PollGroup.cs
+++ PollGroup/PollGroup.cs
@@ -1,26 +1,29 @@
-using System;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 
-namespace System.Network
+namespace System.Network;
+
+public interface IPollGroup : IDisposable
 {
-    public interface IPollGroup : IDisposable
-    {
-        void Add(Socket sock, GCHandle handle);
-        void Remove(Socket sock);
-        int Poll(ref GCHandle[] handles);
-    }
+    void Add(Socket sock, GCHandle handle);
+    void Remove(Socket sock, GCHandle handle);
+    int Poll(int maxEvents);
+    int Poll(int[] fds);
+    int Poll(uint[] u32s);
+    int Poll(ulong[] u64s);
+    int Poll(IntPtr[] ptrs);
+    int Poll(GCHandle[] handles);
+}
 
-    public static class PollGroup
+public static class PollGroup
+{
+    public static IPollGroup Create()
     {
-        public static IPollGroup Create()
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                return new KQueuePollGroup();
-            }
-
-            return new EPollGroup();
+            return new KQueuePollGroup();
         }
+
+        return new EPollGroup();
     }
 }
