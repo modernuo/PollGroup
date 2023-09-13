d506b18b78fd9b972b0f36f1182325498969dc1c
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2023-09-13 09:44:44 -0700

    fix: Fixes Arm64 support

--- PollGroup/EPollGroup.cs
+++ PollGroup/EPollGroup.cs
@@ -5,59 +5,12 @@ namespace System.Network;
 
 public sealed class EPollGroup : IPollGroup
 {
-    [Flags]
-    private enum epoll_flags
+    private enum SupportedArchitecture
     {
-        NONE = 0,
-        CLOEXEC = 0x02000000,
-        NONBLOCK = 0x04000,
-    }
-
-    [Flags]
-    private enum epoll_events : uint
-    {
-        EPOLLIN = 0x001,
-        EPOLLPRI = 0x002,
-        EPOLLOUT = 0x004,
-        EPOLLRDNORM = 0x040,
-        EPOLLRDBAND = 0x080,
-        EPOLLWRNORM = 0x100,
-        EPOLLWRBAND = 0x200,
-        EPOLLMSG = 0x400,
-        EPOLLERR = 0x008,
-        EPOLLHUP = 0x010,
-        EPOLLRDHUP = 0x2000,
-        EPOLLONESHOT = 1 << 30,
-        EPOLLET = unchecked((uint)(1 << 31))
-    }
-
-    private enum epoll_op
-    {
-        EPOLL_CTL_ADD = 1,
-        EPOLL_CTL_DEL = 2,
-        EPOLL_CTL_MOD = 3,
-    }
-
-    [StructLayout(LayoutKind.Explicit, Size = 8)]
-    private struct epoll_data
-    {
-        [FieldOffset(0)]
-        public int fd;
-        [FieldOffset(0)]
-        public IntPtr ptr;
-        [FieldOffset(0)]
-        public uint u32;
-        [FieldOffset(0)]
-        public ulong u64;
-    }
-
-    [StructLayout(LayoutKind.Explicit, Pack = 4)]
-    private struct epoll_event
-    {
-        [FieldOffset(0)]
-        public epoll_events events;
-        [FieldOffset(4)]
-        public epoll_data data;
+        Unknown,
+        Windows,
+        Linux_x64,
+        Linux_arm64
     }
 
     private static class Windows
@@ -69,13 +22,28 @@ public sealed class EPollGroup : IPollGroup
         public static extern int epoll_close(int epfd);
 
         [DllImport("wepoll.dll", SetLastError = true)]
-        public static extern int epoll_ctl(int epfd, epoll_op op, int fd, ref epoll_event ee);
+        public static extern int epoll_ctl(int epfd, epoll_op op, int fd, ref epoll_event_packed ee);
 
         [DllImport("wepoll.dll", SetLastError = true)]
-        public static extern int epoll_wait(int epfd, [In, Out] epoll_event[] ee, int maxevents, int timeout);
+        public static extern int epoll_wait(int epfd, [In, Out] epoll_event_packed[] ee, int maxevents, int timeout);
     }
 
-    private static class Linux
+    private static class Linux_x64
+    {
+        [DllImport("libc", SetLastError = true)]
+        public static extern int epoll_create1(epoll_flags flags);
+
+        [DllImport("libc", SetLastError = true)]
+        public static extern int epoll_close(int epfd);
+
+        [DllImport("libc", SetLastError = true)]
+        public static extern int epoll_ctl(int epfd, epoll_op op, int fd, ref epoll_event_packed ee);
+
+        [DllImport("libc", SetLastError = true)]
+        public static extern int epoll_wait(int epfd, [In, Out] epoll_event_packed[] ee, int maxevents, int timeout);
+    }
+
+    private static class Linux_aarch64
     {
         [DllImport("libc", SetLastError = true)]
         public static extern int epoll_create1(epoll_flags flags);
@@ -91,12 +59,41 @@ public sealed class EPollGroup : IPollGroup
     }
 
     private readonly int _epHndle;
-    private readonly bool _isWindows;
+    private readonly SupportedArchitecture _supportedArchitecture;
+    private readonly bool _isArm;
 
     public EPollGroup()
     {
-        _isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-        _epHndle = _isWindows ? Windows.epoll_create1(epoll_flags.NONE) : Linux.epoll_create1(epoll_flags.NONE);
+        _isArm = RuntimeInformation.ProcessArchitecture is Architecture.Arm or Architecture.Arm64 or Architecture.Armv6;
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            _supportedArchitecture = SupportedArchitecture.Windows;
+        }
+        else if (_isArm)
+        {
+            _supportedArchitecture = SupportedArchitecture.Linux_arm64;
+        }
+        else
+        {
+            _supportedArchitecture = SupportedArchitecture.Linux_x64;
+        }
+
+        _epHndle = _supportedArchitecture switch
+        {
+            SupportedArchitecture.Windows => Windows.epoll_create1(epoll_flags.NONE),
+            SupportedArchitecture.Linux_x64 => Linux_x64.epoll_create1(epoll_flags.NONE),
+            SupportedArchitecture.Linux_arm64 => Linux_aarch64.epoll_create1(epoll_flags.NONE)
+        };
+
+        if (_isArm)
+        {
+            _events = new epoll_event[2048];
+        }
+        else
+        {
+            _eventsPacked = new epoll_event_packed[2048];
+        }
 
         if (_epHndle == 0)
         {
@@ -106,20 +103,53 @@ public sealed class EPollGroup : IPollGroup
 
     public void Dispose()
     {
-        _ = _isWindows ? Windows.epoll_close(_epHndle) : Linux.epoll_close(_epHndle);
+        _ = _supportedArchitecture switch
+        {
+            SupportedArchitecture.Windows     => Windows.epoll_close(_epHndle),
+            SupportedArchitecture.Linux_x64   => Linux_x64.epoll_close(_epHndle),
+            SupportedArchitecture.Linux_arm64 => Linux_aarch64.epoll_close(_epHndle)
+        };
     }
 
     public void Add(Socket socket, GCHandle handle)
     {
-        var ev = new epoll_event
-        {
-            events = epoll_events.EPOLLIN | epoll_events.EPOLLERR
-        };
-        ev.data.ptr = (IntPtr)handle;
+        int rc;
 
-        var rc = _isWindows ?
-            Windows.epoll_ctl(_epHndle, epoll_op.EPOLL_CTL_ADD, (int)socket.Handle, ref ev) :
-            Linux.epoll_ctl(_epHndle, epoll_op.EPOLL_CTL_ADD, (int)socket.Handle, ref ev);
+        if (_isArm)
+        {
+            var ev = new epoll_event
+            {
+                events = epoll_events.EPOLLIN | epoll_events.EPOLLERR
+            };
+            ev.data.ptr = (IntPtr)handle;
+
+            rc = Linux_aarch64.epoll_ctl(_epHndle, epoll_op.EPOLL_CTL_ADD, (int)socket.Handle, ref ev);
+        }
+        else
+        {
+            var ev = new epoll_event_packed
+            {
+                events = epoll_events.EPOLLIN | epoll_events.EPOLLERR
+            };
+            ev.data.ptr = (IntPtr)handle;
+
+            rc = _supportedArchitecture switch
+            {
+                SupportedArchitecture.Windows => Windows.epoll_ctl(
+                    _epHndle,
+                    epoll_op.EPOLL_CTL_ADD,
+                    (int)socket.Handle,
+                    ref ev
+                ),
+                // SupportedArchitecture.Linux_x64
+                _ => Linux_x64.epoll_ctl(
+                    _epHndle,
+                    epoll_op.EPOLL_CTL_ADD,
+                    (int)socket.Handle,
+                    ref ev
+                )
+            };
+        }
 
         if (rc != 0)
         {
@@ -129,15 +159,43 @@ public sealed class EPollGroup : IPollGroup
 
     public void Remove(Socket socket, GCHandle handle)
     {
-        var ev = new epoll_event
-        {
-            events = epoll_events.EPOLLIN | epoll_events.EPOLLERR
-        };
-        ev.data.ptr = (IntPtr)handle;
+        int rc;
 
-        var rc = _isWindows ?
-            Windows.epoll_ctl(_epHndle, epoll_op.EPOLL_CTL_DEL, (int)socket.Handle, ref ev) :
-            Linux.epoll_ctl(_epHndle, epoll_op.EPOLL_CTL_DEL, (int)socket.Handle, ref ev);
+        if (_isArm)
+        {
+            var ev = new epoll_event
+            {
+                events = epoll_events.EPOLLIN | epoll_events.EPOLLERR
+            };
+            ev.data.ptr = (IntPtr)handle;
+
+            rc = Linux_aarch64.epoll_ctl(_epHndle, epoll_op.EPOLL_CTL_ADD, (int)socket.Handle, ref ev);
+        }
+        else
+        {
+            var ev = new epoll_event_packed
+            {
+                events = epoll_events.EPOLLIN | epoll_events.EPOLLERR
+            };
+            ev.data.ptr = (IntPtr)handle;
+
+            rc = _supportedArchitecture switch
+            {
+                SupportedArchitecture.Windows => Windows.epoll_ctl(
+                    _epHndle,
+                    epoll_op.EPOLL_CTL_DEL,
+                    (int)socket.Handle,
+                    ref ev
+                ),
+                // SupportedArchitecture.Linux_x64
+                _ => Linux_x64.epoll_ctl(
+                    _epHndle,
+                    epoll_op.EPOLL_CTL_DEL,
+                    (int)socket.Handle,
+                    ref ev
+                )
+            };
+        }
 
         if (rc != 0)
         {
@@ -145,52 +203,67 @@ public sealed class EPollGroup : IPollGroup
         }
     }
 
-    private epoll_event[] _events = new epoll_event[2048];
+    private epoll_event_packed[] _eventsPacked;
+    private epoll_event[] _events;
 
     public int Poll(int maxEvents)
     {
-        if (maxEvents > _events.Length)
+        if (_isArm)
         {
-            var newLength = Math.Max(maxEvents, _events.Length + (_events.Length >> 2));
-            _events = new epoll_event[newLength];
+            if (maxEvents > _events.Length)
+            {
+                var newLength = Math.Max(maxEvents, _events.Length + (_events.Length >> 2));
+                _events = new epoll_event[newLength];
+            }
+
+            return Linux_aarch64.epoll_wait(_epHndle, _events, maxEvents, 0);
         }
 
-        return _isWindows ?
-            Windows.epoll_wait(_epHndle, _events, maxEvents, 0) :
-            Linux.epoll_wait(_epHndle, _events, maxEvents, 0);
+        if (maxEvents > _events.Length)
+        {
+            var newLength = Math.Max(maxEvents, _eventsPacked.Length + (_eventsPacked.Length >> 2));
+            _eventsPacked = new epoll_event_packed[newLength];
+        }
+
+        return _supportedArchitecture switch
+        {
+            SupportedArchitecture.Windows => Windows.epoll_wait(_epHndle, _eventsPacked, maxEvents, 0),
+            // SupportedArchitecture.Linux_x64
+            _ => Linux_x64.epoll_wait(_epHndle, _eventsPacked, maxEvents, 0)
+        };
     }
 
     public int Poll(GCHandle[] handles)
     {
         var rc = Poll(handles.Length);
-        
+
         if (rc <= 0)
         {
             return rc;
         }
-    
+
         for (var i = 0; i < rc; i++)
         {
             handles[i] = (GCHandle)_events[i].data.ptr;
         }
-    
+
         return rc;
     }
-    
+
     public int Poll(IntPtr[] ptrs)
     {
         var rc = Poll(ptrs.Length);
-        
+
         if (rc <= 0)
         {
             return rc;
         }
-    
+
         for (var i = 0; i < rc; i++)
         {
             ptrs[i] = _events[i].data.ptr;
         }
-    
+
         return rc;
     }
 }
